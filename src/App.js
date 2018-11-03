86193106fa6a60218aea3e6777a2944305034c4e
eyesniper2 <eyesniper2@rayfall.net>
2018-11-03 00:57:39 -0600

    Added interest form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Image, Button } from 'semantic-ui-react'
 import HackEDLogo from './HackEDLogo.svg';
 import About from './Components/About'
 import WhereToGo from './Components/WhereToGo'
@@ -9,6 +9,12 @@ import Sponsors from './Components/Sponsors'
 import './App.css';
 
 class App extends Component {
+
+  redirectToInterestForm = event => {
+    const url = 'https://goo.gl/forms/Q89ck52LzyFSbpti1';
+    window.open(url, '_blank');
+  };
+
   render() {
     return (
       <div className="App">
@@ -17,10 +23,11 @@ class App extends Component {
           centered 
           style={{height: '100vh', background: "linear-gradient(180deg, #FFFFFF 70%, #d02120 90%)"}}>
           <Grid.Row>
-            <Grid.Column width={12} textAlign='center' style={{paddingTop: "20vh"}}>
+            <Grid.Column width={12} textAlign='center' style={{paddingTop: "15vh"}}>
               <Image src={HackEDLogo} fluid />
               <h2>Alberta Largest Student Run Hackathon</h2>
               <h3>January 19 - 20 @ The Donadeo Innovation Centre for Engineering</h3>
+              <Button onClick={this.redirectToInterestForm} color='hackedred' size='huge'>Get notified when applications open!</Button>
             </Grid.Column>
           </Grid.Row>
         </Grid>
