3c8ee948280a9ed0dd392c857e37fdec8786ab29
ShallyBanh <banh.shally@gmail.com>
2018-11-24 16:52:07 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.79739a456a7a52fa6922644cccdc13ef.js"
+  "/precache-manifest.09e35579dd1ba68322fb6815499b7058.js"
 );
 
 workbox.clientsClaim();
