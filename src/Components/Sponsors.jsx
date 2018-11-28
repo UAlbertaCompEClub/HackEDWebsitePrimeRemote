82044248c2c6908a8159984aa272100edaab4d49
eyesniper2 <eyesniper2@rayfall.net>
2018-11-28 00:49:46 -0700

    Added first wave of sponsors

--- src/Components/Sponsors.jsx
+++ src/Components/Sponsors.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 
-import { Grid } from 'semantic-ui-react'
+import { Grid, Image, Divider } from 'semantic-ui-react'
+
+import IntuitLogo from '../logos/Intuit.png'
+
+import StartupEdmontonLogo from '../logos/StartupEdmonton.png'
+import ServusLogo from '../logos/Servus.jpg'
+
+import JobberLogo from '../logos/Jobber.jpg'
 
 class Sponsors extends Component {
     
-
     render() {
         return (
             <Grid 
@@ -14,6 +20,7 @@ class Sponsors extends Component {
                 marginTop: 40,
                 marginBottom: 40
             }}
+            columns={3}
             container
             stackable
             relaxed='very'>
@@ -22,7 +29,48 @@ class Sponsors extends Component {
                         <h1 style={{textAlign: "center", color: "#d02120"}}>SPONSORS</h1>
                     </Grid.Column>
                 </Grid.Row>
-                <Grid.Row centered>
+                <Divider horizontal>TITLE</Divider>
+                <Grid.Row verticalAlign='middle' centered>
+                    <Grid.Column>
+                        <Image
+                            src={IntuitLogo}
+                            fluid
+                            as='a'
+                            href='https://www.intuit.com/'
+                            target='_blank'/>
+                    </Grid.Column>
+                </Grid.Row>
+                <Divider horizontal>GOLD</Divider>
+                <Grid.Row verticalAlign='middle' centered>
+                    <Grid.Column>
+                        <Image
+                            src={ServusLogo}
+                            fluid
+                            as='a'
+                            href='https://www.servus.ca/life'
+                            target='_blank'/>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Image
+                            src={StartupEdmontonLogo}
+                            fluid
+                            as='a'
+                            href='https://www.startupedmonton.com/'
+                            target='_blank'/>
+                    </Grid.Column>
+                </Grid.Row>
+                <Divider horizontal>SILVER</Divider>
+                <Grid.Row verticalAlign='middle' centered>
+                    <Grid.Column>
+                        <Image
+                            src={JobberLogo}
+                            fluid
+                            as='a'
+                            href='https://getjobber.com/'
+                            target='_blank'/>
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row centered columns={1}>
                     <Grid.Column>
                         <h2>SPONSOR A SPECIAL WEEKEND</h2>
                         <h4>Each year, our sponsors help us unite our community of emerging developers, designers, 
