0cd7e2a480756891e32bea2789e9a690e958a971
Shally <shally.banh@devfacto.com>
2018-12-04 17:42:02 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.337ab378126d209fc65ecd90c4ab287d.js"
+  "/precache-manifest.835b03041e84171f4a739315261e44a1.js"
 );
 
 workbox.clientsClaim();
