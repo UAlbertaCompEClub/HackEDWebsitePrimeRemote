9682b13521a90e287fb7b9b865450cd26639509d
Dinula De Silva <dinulasd@gmail.com>
2019-01-20 13:58:59 -0500

    presentation schedule fixed

--- src/Components/PresentationSchedule.jsx
+++ src/Components/PresentationSchedule.jsx
@@ -25,333 +25,333 @@ const presentationSchedule = [
     [
         {
             header: "true",
-            time: "09:00-09:05"
-        },
-        {
-            team: "balloonpowers",
-            time: "09:00-09:05",
-            room: "Banff (DICE 8A-246)"
-        },
-        {
-            team: "BotSpotr",
-            time: "09:00-09:05",
-            room: "Jasper (8-236)"
-        },
-        {
-            team: "WineSuggest",
-            time: "09:00-09:05",
-            room: "Lake Louise (DICE 8A-242)"
-        },
-    ],
-    [
-        {
-            header: "true",
-            time: "09:05-09:10"
-        },
-        {
-            team: "Make-me-laugh",
-            time: "09:05-09:10",
-            room: "Banff (DICE 8A-246)"
-        },
-        {
-            team: "Internet Tracker",
-            time: "09:05-09:10",
-            room: "Jasper (8-236)"
-        },
-        {
-            team: "Search for the Best Garfield Comic",
-            time: "09:05-09:10",
-            room: "Lake Louise (DICE 8A-242)"
-        },
-    ],
-    [
-        {
-            header: "true",
-            time: "09:10-09:15"
-        },
-        {
-            team: "DreamMeme",
-            time: "09:10-09:15",
-            room: "Banff (DICE 8A-246)"
-        },
-        {
-            team: "HACKED2019",
-            time: "09:10-09:15",
-            room: "Jasper (8-236)"
-        },
-        {
-            team: "shouldnt.js",
-            time: "09:10-09:15",
-            room: "Lake Louise (DICE 8A-242)"
-        },
-    ],
-    [
-        {
-            header: "true",
-            time: "09:15-09:20"
-        },
-        {
-            team: "Feelsbook",
-            time: "09:15-09:20",
-            room: "Banff (DICE 8A-246)"
-        },
-        {
-            team: "HackED2019",
-            time: "09:15-09:20",
-            room: "Jasper (8-236)"
-        },
-        {
-            team: "HackED-2019-Not!!EchoSock",
-            time: "09:15-09:20",
-            room: "Lake Louise (DICE 8A-242)"
-        },
-    ],
-    [
-        {
-            header: "true",
-            time: "09:20-09:25"
-        },
-        {
-            team: "EulerTikz",
-            time: "09:20-09:25",
-            room: "Banff (DICE 8A-246)"
-        },
-        {
-            team: "uAlberta5Star",
-            time: "09:20-09:25",
-            room: "Jasper (8-236)"
-        },
-        {
-            team: "Profolio Hack",
-            time: "09:20-09:25",
-            room: "Lake Louise (DICE 8A-242)"
-        },
-    ],
-    [
-        {
-            header: "true",
-            time: "09:25-09:30"
-        },
-        {
-            team: "Divergence",
-            time: "09:25-09:30",
-            room: "Banff (DICE 8A-246)"
-        },
-        {
-            team: "EyeMouse",
-            time: "09:25-09:30",
-            room: "Jasper (8-236)"
-        },
-        {
-            team: "petah-royale",
-            time: "09:25-09:30",
-            room: "Lake Louise (DICE 8A-242)"
-        },
-    ],
-    [
-        {
-            header: "true",
-            time: "09:30-09:35"
-        },
-        {
-            team: "Rover the Explorer",
-            time: "09:30-09:35",
-            room: "Banff (DICE 8A-246)"
+            time: "12:45-12:50"
         },
         {
             team: "MovieBot",
-            time: "09:30-09:35",
-            room: "Jasper (8-236)"
-        },
-        {
-            team: "One Stop Photoshop",
-            time: "09:30-09:35",
-            room: "Lake Louise (DICE 8A-242)"
-        },
-    ],
-    [
-        {
-            header: "true",
-            time: "09:35-09:40"
-        },
-        {
-            team: "Beat The Odds",
-            time: "09:35-09:40",
+            time: "00:45-00:50",
             room: "Banff (DICE 8A-246)"
-        },
-        {
-            team: "Boppo",
-            time: "09:35-09:40",
-            room: "Jasper (8-236)"
-        },
-        {
-            team: "Geographic News Contextualizer",
-            time: "09:35-09:40",
-            room: "Lake Louise (DICE 8A-242)"
-        },
-    ],
-    [
-        {
-            header: "true",
-            time: "09:40-09:45"
-        },
-        {
-            team: "course prerequisites",
-            time: "09:40-09:45",
-            room: "Banff (DICE 8A-246)"
-        },
-        {
-            team: "Shade",
-            time: "09:40-09:45",
-            room: "Jasper (8-236)"
-        },
-        {
-            team: "Student Gradebook",
-            time: "09:40-09:45",
-            room: "Lake Louise (DICE 8A-242)"
-        },
-    ],
-    [
-        {
-            header: "true",
-            time: "09:45-09:50"
         },
         {
             team: "git-emotional",
-            time: "09:45-09:50",
-            room: "Banff (DICE 8A-246)"
-        },
-        {
-            team: "ARMageddon",
-            time: "09:45-09:50",
+            time: "00:45-00:50",
             room: "Jasper (8-236)"
         },
         {
-            team: "UofA-advisor",
-            time: "09:45-09:50",
+            team: "EyeMouse",
+            time: "00:45-00:50",
             room: "Lake Louise (DICE 8A-242)"
         },
     ],
     [
         {
             header: "true",
-            time: "09:50-09:55"
+            time: "12:50-12:55"
         },
         {
-            team: "CloudCity",
-            time: "09:50-09:55",
+            team: "ARMageddon",
+            time: "00:50-00:55",
             room: "Banff (DICE 8A-246)"
         },
         {
             team: "Room Searcher",
-            time: "09:50-09:55",
+            time: "00:50-00:55",
             room: "Jasper (8-236)"
+        },
+        {
+            team: "Profolio Hack",
+            time: "00:50-00:55",
+            room: "Lake Louise (DICE 8A-242)"
+        },
+    ],
+    [
+        {
+            header: "true",
+            time: "12:55-01:00"
         },
         {
             team: "Android-App-1",
-            time: "09:50-09:55",
-            room: "Lake Louise (DICE 8A-242)"
-        },
-    ],
-    [
-        {
-            header: "true",
-            time: "09:55-10:00"
-        },
-        {
-            team: "Space Jump",
-            time: "09:55-10:00",
+            time: "00:55-01:00",
             room: "Banff (DICE 8A-246)"
         },
         {
-            team: "Yah-fi-API",
-            time: "09:55-10:00",
+            team: "Space Jump",
+            time: "00:55-01:00",
             room: "Jasper (8-236)"
         },
         {
-            team: "Roommates",
-            time: "09:55-10:00",
+            team: "UofA-advisor",
+            time: "00:55-01:00",
             room: "Lake Louise (DICE 8A-242)"
         },
     ],
     [
         {
             header: "true",
-            time: "10:00-10:05"
+            time: "01:00-01:05"
         },
         {
-            team: "Pipe",
-            time: "10:00-10:05",
+            team: "uAlberta5Star",
+            time: "01:00-01:05",
+            room: "Banff (DICE 8A-246)"
+        },
+        {
+            team: "Make-me-laugh",
+            time: "01:00-01:05",
+            room: "Jasper (8-236)"
+        },
+        {
+            team: "Feelsbook",
+            time: "01:00-01:05",
+            room: "Lake Louise (DICE 8A-242)"
+        },
+    ],
+    [
+        {
+            header: "true",
+            time: "01:05-01:10"
+        },
+        {
+            team: "CloudCity",
+            time: "01:05-01:10",
+            room: "Banff (DICE 8A-246)"
+        },
+        {
+            team: "balloonpowers",
+            time: "01:05-01:10",
+            room: "Jasper (8-236)"
+        },
+        {
+            team: "ChemARstry",
+            time: "01:05-01:10",
+            room: "Lake Louise (DICE 8A-242)"
+        },
+    ],
+    [
+        {
+            header: "true",
+            time: "01:10-01:15"
+        },
+        {
+            team: "shouldnt.js",
+            time: "01:10-01:15",
+            room: "Banff (DICE 8A-246)"
+        },
+        {
+            team: "Search for the Best Garfield Comic",
+            time: "01:10-01:15",
+            room: "Jasper (8-236)"
+        },
+        {
+            team: "DreamMeme",
+            time: "01:10-01:15",
+            room: "Lake Louise (DICE 8A-242)"
+        },
+    ],
+    [
+        {
+            header: "true",
+            time: "01:15-01:20"
+        },
+        {
+            team: "HackED-2019-Not!!EchoSock",
+            time: "01:15-01:20",
+            room: "Banff (DICE 8A-246)"
+        },
+        {
+            team: "WhatWear",
+            time: "01:15-01:20",
+            room: "Jasper (8-236)"
+        },
+        {
+            team: "EulerTikz",
+            time: "01:15-01:20",
+            room: "Lake Louise (DICE 8A-242)"
+        },
+    ],
+    [
+        {
+            header: "true",
+            time: "01:20-01:25"
+        },
+        {
+            team: "Internet Tracker",
+            time: "01:20-01:25",
             room: "Banff (DICE 8A-246)"
         },
         {
             team: "Crowd Density Estimator",
-            time: "10:00-10:05",
+            time: "01:20-01:25",
             room: "Jasper (8-236)"
         },
         {
-            team: "Tuber",
-            time: "10:00-10:05",
+            team: "Student Gradebook",
+            time: "01:20-01:25",
             room: "Lake Louise (DICE 8A-242)"
         },
     ],
     [
         {
             header: "true",
-            time: "10:05-10:10"
+            time: "01:25-01:30"
         },
         {
             team: "Gesture Controlled Desktop Robotic Arm",
-            time: "10:05-10:10",
+            time: "01:25-01:30",
             room: "Banff (DICE 8A-246)"
         },
         {
-            team: "RapidRenter",
-            time: "10:05-10:10",
+            team: "Yah-fi-API",
+            time: "01:25-01:30",
+            room: "Jasper (8-236)"
+        },
+        {
+            team: "Pipe",
+            time: "01:25-01:30",
+            room: "Lake Louise (DICE 8A-242)"
+        },
+    ],
+    [
+        {
+            header: "true",
+            time: "01:30-01:35"
+        },
+        {
+            team: "course prerequisites",
+            time: "01:30-01:35",
+            room: "Banff (DICE 8A-246)"
+        },
+        {
+            team: "petah-royale",
+            time: "01:30-01:35",
+            room: "Jasper (8-236)"
+        },
+        {
+            team: "BotSpotr",
+            time: "01:30-01:35",
+            room: "Lake Louise (DICE 8A-242)"
+        },
+    ],
+    [
+        {
+            header: "true",
+            time: "01:35-01:40"
+        },
+        {
+            team: "Tuber",
+            time: "01:35-01:40",
+            room: "Banff (DICE 8A-246)"
+        },
+        {
+            team: "Boppo",
+            time: "01:35-01:40",
             room: "Jasper (8-236)"
         },
         {
             team: "Eraser",
-            time: "10:05-10:10",
+            time: "01:35-01:40",
             room: "Lake Louise (DICE 8A-242)"
         },
     ],
     [
         {
             header: "true",
-            time: "10:10-10:15"
+            time: "01:40-01:45"
         },
         {
             team: "MyHealth",
-            time: "10:10-10:15",
+            time: "01:40-01:45",
             room: "Banff (DICE 8A-246)"
         },
         {
-            team: "ChemARstry",
-            time: "10:10-10:15",
+            team: "WineSuggest",
+            time: "01:40-01:45",
             room: "Jasper (8-236)"
         },
         {
             team: "enigma-shell",
-            time: "10:10-10:15",
+            time: "01:40-01:45",
             room: "Lake Louise (DICE 8A-242)"
         },
     ],
     [
         {
             header: "true",
-            time: "10:15-10:20"
+            time: "01:45-01:50"
         },
         {
-            team: "WhatWear",
-            time: "10:15-10:20",
+            team: "One Stop Photoshop",
+            time: "01:45-01:50",
+            room: "Banff (DICE 8A-246)"
+        },
+        {
+            team: "Geographic News Contextualizer",
+            time: "01:45-01:50",
+            room: "Jasper (8-236)"
+        },
+        {
+            team: "Rover the Explorer",
+            time: "01:45-01:50",
+            room: "Lake Louise (DICE 8A-242)"
+        },
+    ],
+    [
+        {
+            header: "true",
+            time: "01:50-01:55"
+        },
+        {
+            team: "HACKED2019",
+            time: "01:50-01:55",
+            room: "Banff (DICE 8A-246)"
+        },
+        {
+            team: "HackED2019",
+            time: "01:50-01:55",
+            room: "Jasper (8-236)"
+        },
+        {
+            team: "Shade",
+            time: "01:50-01:55",
+            room: "Lake Louise (DICE 8A-242)"
+        },
+    ],
+    [
+        {
+            header: "true",
+            time: "01:55-02:00"
+        },
+        {
+            team: "Roommates",
+            time: "01:55-02:00",
+            room: "Banff (DICE 8A-246)"
+        },
+        {
+            team: "Beat The Odds",
+            time: "01:55-02:00",
+            room: "Jasper (8-236)"
+        },
+        {
+            team: "Divergence",
+            time: "01:55-02:00",
+            room: "Lake Louise (DICE 8A-242)"
+        },
+    ],
+    [
+        {
+            header: "true",
+            time: "02:00-02:05"
+        },
+        {
+            team: "RapidRenter",
+            time: "02:00-02:05",
             room: "Banff (DICE 8A-246)"
         },
         {
         },
         {
         },
-    ]    
+    ]        
 ];
 
 class PresentationSchedule extends Component {
